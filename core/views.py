120dbea8e36fb2c6f368ebf1b1c9298d283f0030
salesamir <americanoamir@gmail.com>
2025-11-15 15:17:16 -0300

    ajuste nos requerements

--- core/views.py
+++ core/views.py
@@ -5,4 +5,4 @@ def teste(request):
     return HttpResponse("Olá, Mundo! Esta é a minha primeira view no Django.")
 
 def teste2(request):
-    return HttpResponse("la ele")
+    return HttpResponse("teste")
